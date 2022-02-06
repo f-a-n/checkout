40357e2cfcae692f207d2f154b2fd47a8a193ae5
f-a-n <46341223+f-a-n@users.noreply.github.com>
2022-02-07 09:37:11 +1100

    This is my first commit

---
+++ index.js
@@ -0,0 +1,58 @@
+// Hardcoded array of items:
+const itemsForSale = [{
+    name: "iPhone 13",
+    img: "/product-images/1.jpg",
+    price: 1249, 
+    id: 1,
+    },{
+    name: "iPhone 13 Pro",
+    img: "/product-images/2.jpg",
+    price: 1699, 
+    id: 2,
+    },{
+    name: "iPhone 13 Pro Max",
+    img: "/product-images/3.jpg",
+    price: 2199, 
+    id: 3,
+    }, {
+    name: "iPhone 13 Mini",
+    img: "/product-images/4.jpg",
+    price: 1199, 
+    id: 4,
+    }
+];
+
+const productContainer = document.getElementById("product-container")
+
+itemsForSale.map((item) => {
+    const itemDiv = document.createElement("div");
+    itemDiv.innerHTML = `
+    <img src="${item.img}">
+    <div>${item.name}</div>
+    <div>${item.price}</div>
+    `;
+    itemDiv.classList.add("item");
+    itemDiv.dataset.id = item.id;
+    
+    // DEBUGGING:
+    console.log(itemDiv.innerHTML);
+
+    // The add to cart button:
+    const itemButton = document.createElement("button");
+    itemButton.innerHTML = "add to cart"; 
+    itemButton.addEventListener("click", (e) => {
+        console.log(e.target.parentElement.dataset.id);
+    })
+
+    itemDiv.appendChild(itemButton);
+    productContainer.appendChild(itemDiv)  
+});
+
+// loop through the products array, add them to the DOM.
+
+//  Next job:
+// Cart array
+// Cart object, with cart methods:
+    // delete item
+    // display cart (?)
+    // 
